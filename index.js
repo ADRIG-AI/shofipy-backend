23414106287f2c268b28544ecb7dfd49c6d39f25
Sugrish V M <153385658+sug2004@users.noreply.github.com>
2025-07-06 19:09:47 +0530

    added fronend url

--- index.js
+++ index.js
@@ -35,41 +35,40 @@
 //     console.log(`Health check: http://localhost:${port}`);
 //     console.log(`Webhook endpoint: http://localhost:${port}/api/webhook`);
 // });
-
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import apiRoutes from './routes/index.js';
 import { webhookController } from './controllers/webhook/stripeController.js';
+import { callbackController } from './controllers/auth/callbackController.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// CORS setup
 const corsOptions = {
-  origin: 'http://localhost:8080'|| 'https://shopify-frontend-pearl.vercel.app/', // change to your frontend URL
+  origin: [
+    'http://localhost:8080',
+    'https://shopify-frontend-pearl.vercel.app'
+  ],
   credentials: true,
 };
 app.use(cors(corsOptions));
 
-// Stripe webhook (MUST come before bodyParser.json)
 app.post('/api/webhook', express.raw({ type: 'application/json' }), webhookController);
 
-// Apply body-parser for JSON with increased size limit
 app.use(bodyParser.json({ limit: '25mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '25mb' }));
 
-// Mount API routes
+app.get('/auth/callback', callbackController);
+
 app.use('/api', apiRoutes);
 
-// Basic health check
 app.get('/', (req, res) => {
   res.json({ status: 'Stripe webhook server is running!' });
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`🚀 Server is running on http://localhost:${port}`);
   console.log(`🧪 Health check:     http://localhost:${port}/`);
