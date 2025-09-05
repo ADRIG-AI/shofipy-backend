ec11e39893d5dc226d816658a98f1222c2baf7f7
Sugrish V M <153385658+sug2004@users.noreply.github.com>
2025-09-05 12:09:27 +0530

    fixed cors

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const corsOptions = {
   origin: [
     'http://localhost:8080',
     'https://shopify-frontend-pearl.vercel.app',
-    'https://shopify-frontend-rouge.vercel.app'
+    'https://shopify-frontend-rouge.vercel.app',
+    'https://www.shopifyq.com'
   ],
   credentials: true,
 };
