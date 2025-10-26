21302d87c3e7fea3916495d0ea71c7768fb4f66e
Sugrish V M <153385658+sug2004@users.noreply.github.com>
2025-10-26 22:47:54 +0530

    add new CORS origin for Dagala Analytics

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ const corsOptions = {
     'http://localhost:8080',
     'https://shopify-frontend-pearl.vercel.app',
     'https://shopify-frontend-rouge.vercel.app',
-    'https://www.shopifyq.com'
+    'https://www.shopifyq.com',
+    'https://dagala-analytics.vercel.app'
   ],
   credentials: true,
 };
